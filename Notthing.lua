f7e6ae20b94010986db9106886b9c3de23457f91
dumanhucac69 <dumanhucac71@gmail.com>
2025-09-19 19:13:37 +0700

    Notthing.lua
    Notthing script key: Notthing303

--- Notthing.lua
+++ Notthing.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/dumanhucac69/Notthing-hub/refs/heads/main/Notthing.lua")))()
 
        local Window = MakeWindow({
          Hub = {
