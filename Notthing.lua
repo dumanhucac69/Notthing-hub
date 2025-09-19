6df7fe56295476c926c2b0611318758d22dc5c38
dumanhucac69 <dumanhucac71@gmail.com>
2025-09-19 19:14:12 +0700

    Notthing.lua
    Script roblox key: Notthing303

--- Notthing.lua
+++ Notthing.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/dumanhucac69/Notthing-hub/refs/heads/main/Notthing.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/dumanhucac69/Notthing-hub/refs/heads/main/Notthing.lua"))()
 
        local Window = MakeWindow({
          Hub = {
